c3a0fb88d94cedcafd3d72989ffc6238ce84296f
nattzchukwumela <chukwumelapraise@gmail.com>
2025-11-05 14:31:05 +0100

    added image our clients say section

--- src/component/main.tsx
+++ src/component/main.tsx
@@ -122,22 +122,26 @@ export default function BarbershopWebsite() {
                   quote:
                     "Your hair is more than just strands; it's a living tapestry woven with your experiences, memories, and your own unique spirit. We don't just cut hair; we here you tell your story through it and nurture it with care tailored to your own style, own your dream.",
                   author: "~BBS Concepts~",
+                  img: "public/img/service/show-off/Square37.jpg",
                 },
                 {
                   quote:
                     "More than just strands, it's an expression of your inner artist. We provide the canvas, the tools, and the expertise to help you craft a masterpiece that radiates both confidence and individuality. Step into our salon ready to transform, and leave ready to conquer your truly life.",
                   author: "~BBS Concepts~",
+                  img: "public/img/service/show-off/Square32.jpg",
                 },
                 {
                   quote:
                     "Escape the everyday hustle and bustle and indulge in a transformative cut that transcends the ordinary. Step into our sanctuary where our expert guided hands and soothing products work their magic. Let our artists craft a style that not only reflects your inner peace and leaves you feeling pampered and revitalized.",
                   author: "~BBS Concepts~",
+                  img: "public/img/service/show-off/Square33.jpg",
                 },
               ].map((testimonial, idx) => (
                 <div key={idx} className="testimonial-card">
                   <div className="testimonial-img">
-                    <div className="placeholder-icon">✂️</div>
-                    <p>Image {idx + 1}</p>
+                    <div className="img-container">
+                      <img src={testimonial.img} alt="" className="img" />
+                    </div>
                   </div>
                   <p className="testimonial-quote">{testimonial.quote}</p>
                   <p className="testimonial-author">{testimonial.author}</p>
